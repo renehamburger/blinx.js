9b9a6f3b5718395a99c320f7b29602f27edcb797
Rene Hamburger <rene.hamburger@leanix.net>
2018-03-27 23:07:40 +0200

    Move to ES3 target
    for potential support of older browsers

--- src/helpers/deferred.class.ts
+++ src/helpers/deferred.class.ts
@@ -6,19 +6,15 @@ export type Resolver<T> = (value?: T | PromiseLike<T>) => void;
 export type Rejecter = (reason?: any) => void;
 
 export class Deferred<T> {
-  private _promise: Promise<T>;
+  public readonly promise: Promise<T>;
   private _resolve?: Resolver<T>;
   private _reject?: Rejecter;
 
   constructor() {
-    this._promise = new Promise<T>((resolve: Resolver<T>, reject: Rejecter) => {
+    this.promise = new Promise<T>((resolve: Resolver<T>, reject: Rejecter) => {
       this._resolve = resolve;
       this._reject = reject;
     });
-  }
-
-  get promise(): Promise<T> {
-    return this._promise;
   }
 
   resolve = (value?: T | PromiseLike<T>): void => {
