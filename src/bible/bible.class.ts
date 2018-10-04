f64d4e898e5db860b088174554c8a20dd49a18f0
Rene Hamburger <rene.hamburger@gmail.com>
2018-10-04 21:50:33 +0200

    Remove logging from unit tests & add to do

--- src/bible/bible.class.ts
+++ src/bible/bible.class.ts
@@ -15,6 +15,8 @@ export abstract class Bible {
     const availableVersions: Partial<BibleVersionsInterface> = {};
     for (const version in bibleVersions) {
       if (bibleVersions.hasOwnProperty(version)) {
+        // TODO: Use 'in' operator?
+        // Also: this is called for every link and iterates through all versions: can be improved.
         if (Object.keys(this.bibleVersionMap).indexOf(version) > -1) {
           if (!language || language === bibleVersions[version as BibleVersionCode].languageCode) {
             availableVersions[version as BibleVersionCode] = bibleVersions[version as BibleVersionCode];
