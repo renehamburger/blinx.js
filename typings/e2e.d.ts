dc0f18e81cf59e9b06eb0a9da7f6d03b759d8758
Rene Hamburger <rene.hamburger@gmail.com>
2021-03-28 17:57:17 +0200

    Provide separate type definitions for each tsconfig file
    This prevents jasmine or jest types being loaded for other *.ts files
    and avoids the duplication of the require() typing.

---
+++ typings/e2e.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="../node_modules/typescript/lib/lib.d.ts" />
+/// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts" />
+/// <reference path="./shared/index.d.ts" />
+/// <reference types="jasmine" />
+
+declare const require: (module: string) => any;
